c9e02e96a059aae41e1c56bf80993e4189ea3ede
Solek <Solek@users.noreply.github.com>
2016-03-01 14:53:43 +0300

    Turning programm into C++ step-by-step from C/C++
    Quite a big update. Forgot to git.
    
    line "using namespace std;" was replaced by "std::" prefixes where it was necessary.
    fgets() was replaced by getline().
    printf() / scanf() were replaced by cout / cin.
    * FILE commands were replaced by ifstream/ofstream, necessary libs were added.
    Useless libs were removed.
    
    Code looks better but has lost functionality: currently programm is compiled, but don't really work. Bug is somewhere in taking data from the file.

--- cubes1.cpp
+++ cubes1.cpp
@@ -1,55 +1,60 @@
-#include <stdio.h>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <string>
 #include <cstring>
 
-using namespace std;
-
 int main()
-{
+	{
 	float length, width, height, total;
 	float length_of_min, width_of_min, height_of_min;
 	float min_total = 10000000;
 	char name[20];
 	char name_of_min[20];
-	char temp_line[40];
+	char temp_line[50];
 	char filename_i[256];
 	char filename_o[256];
-
-	cout << "Type name of your file please: \n";
+	
+	std::cout << "Type name of your file please: \n";
 	std::cin.getline(filename_i, 256);
-	cout << "\nThanks! Now, how your output file should be called? \n";
+	std::cout << "\nThanks! Now, how your output file should be called? \n";
 	std::cin.getline(filename_o, 256);
-	cout << "\n";
+	std::cout << "\n";
+	
+	std::ifstream input;
+	input.open(filename_i);
+	std::ofstream output;
+	output.open(filename_o);
 
-	FILE * input;
-	input = fopen(filename_i, "rt");
-    FILE * output;
-    output = fopen(filename_o, "wt");
+	
+	while(input.eof())
+		{
+			std::stringstream ss_line;
+			std::string temp_line;
+    		getline(input, temp_line);
+    
+			for (std::size_t i = 0; i < temp_line.length(); ++i) 
+			
+			ss_line << temp_line;
+			ss_line >> name >> length >> width >> height;
+			std::cout << name << " " << length << " " << width << " " << height <<'\n';
+			total = 2 * (length * height + length * width + height * width);
+			if (length > 0 && width > 0 && height > 0 && total < min_total)
+			{
+				strcpy(name_of_min, name);
+				length_of_min = length;
+				width_of_min = width;
+				height_of_min = height;
+				min_total = total;
+			}
+		}
+	
+output << "Smallest square between all existing objects is " << min_total << "!\n\n";
+output << "Name of object is \"" << name_of_min << "\". Length is " << length_of_min <<", width is " << width_of_min <<" and height is " << height_of_min << ".\n";
 
-    while (fgets(temp_line, 200, input) != NULL)
-    	{
-    	std::stringstream ss_line;
+input.close();
+output.close();
 
-    	ss_line << temp_line;
-    	ss_line >> name >> length >> width >> height;
-    	std::cout << name << " " << length << " " << width << " " << height <<'\n';
-    	total = 2 * (length * height + length * width + height * width);
-        if (length > 0 && width > 0 && height > 0 && total < min_total)
-        	{
-        	strcpy(name_of_min, name);
-        	length_of_min = length;
-        	width_of_min = width;
-        	height_of_min = height;
-        	min_total = total;
-        	}
-    	}
+return 0;
 
-    fprintf(output, "Smallest square between all existing objects is %.3f!\n\n", min_total);
-    fprintf(output, "Name of object is \"%s\". Length is %.3f, width is %.3f and height is %.3f.\n", name_of_min, length_of_min, width_of_min, height_of_min);
-
-    fclose(input);
-    fclose(output);
-    return 0;
-}
+	}
